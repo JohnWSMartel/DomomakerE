1544183bf6f708c62aa64c818d5444455644c1c1
JohnWSMartel <jwm3464@g.rit.edu>
2019-04-04 16:13:08 -0400

    Added csrf token to delete form

--- client/app/maker.js
+++ client/app/maker.js
@@ -58,9 +58,10 @@ const DomoList = function(props) {
 				<img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
 				<h3 className="domoName"> Name: {domo.name} </h3>
 				<h3 className="domoAge"> Age: {domo.age} </h3>
-                <h3 className="domoLevel"> Level: {domo.level} </h3>
-                <input type="hidden" name="_id" value="" />
-                <input type="submit" value="Delete Domo" />
+        <h3 className="domoLevel"> Level: {domo.level} </h3>
+        <input type="hidden" name="_id" value="" />
+				<input type="hidden" name="_csrf" value={props.csrf} />
+        <input type="submit" value="Delete Domo" />
 			</div>
           </form>
 		);
